dd17c585acf09cc9d596dbe4f07685a279cb5edf
Andrea Sánchez Tapia <asanchezt@unal.edu.co>
2023-02-09 19:55:43 -0800

    update download

--- R_scripts/06_g_drive.R
+++ R_scripts/06_g_drive.R
@@ -1,22 +1,14 @@
+# Download data created by GEE to Drive----
+
 library(googledrive)
 googledrive::drive_auth() #dar acessso desde o navegador
-# Criando a pasta para não sobreescrever processed_data:
+
+## Criando a pasta para não sobreescrever processed_data:----
 dir.create("drive_data/landform_variety", recursive = TRUE)
 
-# Look up landform variety
-lf_var <- drive_find(pattern = "landform_variety")
 
-#Download from drive using the ids: 
-for (i in seq_along(lf_var$id)) {
-  id <- lf_var$id[i]
-  googledrive::drive_download(file = as_id(id),
-                              path = paste0("drive_data/landform_variety_", i),
-                              overwrite = TRUE)
-}
-
-
-# Look up landform variety
-## maybe with a function so we don't copy code
+## Look up landform variety and download by id----
+## with a function so we don't copy code
 download_results <- function(pattern = "landform_variety") {
   lf_var <- drive_find(pattern = pattern)
   #Download from drive using the ids: 
@@ -27,6 +19,10 @@ download_results <- function(pattern = "landform_variety") {
                                 overwrite = TRUE)
     }
 }
+download_results("landform_variety")# default
 download_results("Brazil_landform_types")
+download_results("elevation_range_residual")
+download_results("soil_diversity")
+# aqui falta agua pelo asset do Lucas. ----
 
 # NOTE: We have to ignore this folder to avoid UPLOADING back to GEE. It will kill our quota. 
